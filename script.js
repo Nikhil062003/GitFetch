8f39b925fa5d77edbb601606223804e14f41a45e
NIKHIL SHARMA <77260834+Nikhil062003@users.noreply.github.com>
2023-07-04 12:52:50 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ let following=document.getElementById("following");
 let respo=document.getElementById("respo");
 let pull=document.getElementById("pull");
 let ro=document.getElementById("repo");
+
 let count=0;
 
 
@@ -22,10 +23,9 @@ else{
     let obj=JSON.parse(request.responseText);
    
       img.setAttribute("src",obj.avatar_url);
-      img.setAttribute("style","width:200px;")
       login.innerHTML=`${obj.name} @${obj.login}`;
-      following.innerHTML="following:- "+obj.following;
-      followers.innerHTML="followers:- "+obj.followers;
+      following.innerHTML="Following:- "+obj.following;
+      followers.innerHTML="Followers:- "+obj.followers;
      
     
   })
@@ -38,14 +38,14 @@ let rrequest=new XMLHttpRequest();
     
     obj.forEach(function(repo){
       let span=document.createElement("p");
-      span.setAttribute("style","background-color:orange;width:200px");
+      // span.setAttribute("style","background-color:orange;width:200px");
       span.innerHTML=repo.name;
      
       respo.appendChild(span);
        count++;
        
     })
-   ro.innerHTML="repo:- " +count;
+   ro.innerHTML="Total Repositories:- "+count;
   })
   
   rrequest.send();
